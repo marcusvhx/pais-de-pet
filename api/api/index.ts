30db34300edd907079f0739036936f5ae29efe6c
marcushxavier <marcusvyinicius@gmail.com>
2025-08-29 00:38:45 -0300

    atualizando o tsconfig

--- api/api/index.ts
+++ api/api/index.ts
@@ -1,6 +1,6 @@
-import { createServer } from "http"
-import app from "../src/server"
-export default (req, res)=>{
-    const server = createServer((req_,res_)=>app(req_,res_))
-    server.emit("request",req,res)
-}
+import { createServer } from "http";
+import app from "../src/server";
+export default (req: any, res: any) => {
+  const server = createServer((req_, res_) => app(req_, res_));
+  server.emit("request", req, res);
+};
